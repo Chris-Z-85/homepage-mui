72d943eaa25cb9006d7a254d6d08be69cc751b9f
Krzysztof Zbrojkiewicz <chris.z@aol.com>
2021-12-17 12:41:42 +0000

    create hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import Grid from "@mui/material/Grid";
+import Chris from "../images/chris_z.svg";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+
+const Hero = () => {
+  return (
+    <Grid container justifyContent="center" sx={{ height: "100vh" }}>
+      <Grid item>
+        <Grid
+          container
+          justifyContent="space-between"
+          alignItems="center"
+          sx={{ width: "70vw", height: "80vh" }}
+        >
+          <Grid item>
+            <Typography variant="h3" component="div" gutterBottom>
+              Hey, my name is
+            </Typography>
+            <Typography variant="h2" component="div" gutterBottom>
+              Chris Z.
+            </Typography>
+            <Typography variant="h3" component="div" gutterBottom>
+              I'm Frontend Developer
+            </Typography>
+            <Typography variant="h4" component="div" gutterBottom>
+              I build websites.
+            </Typography>
+            <Stack spacing={2} direction="row">
+              <Button variant="contained">
+                Hire me <ArrowForwardIosIcon fontSize="1rem" />
+              </Button>
+              <Button variant="outlined">
+                My works <ArrowForwardIosIcon fontSize="1rem" />
+              </Button>
+            </Stack>
+          </Grid>
+          <Grid item xs={6} md={4}>
+            <img src={Chris} alt="Chris Z." />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Hero;
